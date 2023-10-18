2c6defddd06062aa5ecced3f728e124ab5a1c6a8
Lionel Miranda <bibliothecalionel@gmail.com>
2023-10-18 17:38:25 -0400

    feat: anagram with a custom counting sort algorithm

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,29 @@
+def custom_c_sort(string):
+    # cria uma lista com 26 posições, uma para cada letra do alfabeto.
+    counts = [0] * 26
+
+    # para cada letra na string, incrementa o contador da letra correspondente.
+    for char in string:
+        char = char.lower()
+        # vai adicionar +1 no counts
+        # , na posição da letra correspondente ao alfabeto.
+        counts[ord(char) - ord('a')] += 1
+
+    # cria uma lista com o número de ocorrências de cada letra.
+    # a saída é algo assim ['aa', '', '', 'b', '', '', ..., '']
+    result = [chr(i + ord('a')) * counts[i] for i in range(26)]
+
+    # retorna a lista concatenada em uma string.
+    return ''.join(result)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # se uma das strings for vazia, retorna False.
+    if not first_string or not second_string:
+        return custom_c_sort(first_string), custom_c_sort(second_string), False
+
+    sorted_first = custom_c_sort(first_string)
+    sorted_second = custom_c_sort(second_string)
+
+    # retorno ordenado em uma tupla.
+    return sorted_first, sorted_second, sorted_first == sorted_second
