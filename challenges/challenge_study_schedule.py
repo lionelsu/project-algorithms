ea9ebd7f2f01c9506cd24bee4c5cfe19fde142b8
Lionel Miranda <bibliothecalionel@gmail.com>
2023-10-18 13:34:18 -0400

    feat: student schedule algorithms

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+    count = 0
+
+    for start, finish in permanence_period:
+        if not isinstance(start, int) or not isinstance(finish, int):
+            return None
+        if start <= target_time <= finish:
+            count += 1
+    return count
