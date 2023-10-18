322d122a29535db195db38aca36fc1182f59e7a5
Lionel Miranda <bibliothecalionel@gmail.com>
2023-10-18 15:54:01 -0400

    feat: palindrome algorithms recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
+# uma solução recursiva chama a si mesma, uma solução iterativa usa um loop.
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word or word == '':
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if low_index >= high_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
